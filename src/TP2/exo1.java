bebeae1b29e7a0164dd6baf111c1b37eb20c4d68
Ouma <oumayma.boumous@gmail.com>
2024-03-08 16:15:14 +0100

    TP

--- src/TP2/exo1.java
+++ src/TP2/exo1.java
@@ -1,5 +1,36 @@
 package TP2;
-
+import java.util.Scanner;
 public class exo1 {
-
+    public static void main(String[] args) {
+        System.out.print("Veuillez entrer le nombre des entiers N: ");
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int i=1,sum=0,p=1;
+      /*  while(i<N){
+            i++;
+            System.out.print("Entrer l'entier: " +i);
+            int m=sc.nextInt();
+            sum+=m;
+            p*=m;
+        }
+        sc.close();
+        System.out.println("La somme est : "+sum+"\nLa moyenne est :" +(sum/N)+"\nLe produit est : "+p);
+do {
+    i++;
+    System.out.print("Veuillez entrer l'entier: "+i);
+    int m=sc.nextInt();
+    sum+=m;
+    p*=m;
+}while(i<N);
+        sc.close();
+        System.out.println("La somme est : "+sum+"\nLa moyenne est: "+(sum/n)+"\nLe produit est: "+p);*/
+        for(i=1;i<=N;i++){
+            System.out.print("Veuillez entrer l'entier: "+i);
+            int m=sc.nextInt();
+            sum+=m;
+            p*=m;
+        }
+        sc.close();
+        System.out.println("La somme est: "+sum+"\nLa moyenne est : "+(sum/N)+"\nLe produit est: "+p);
+    }
 }
--- src/TP2/exo2.java
+++ src/TP2/exo2.java
@@ -1,4 +1,15 @@
 package TP2;
-
+import java.util.Scanner;
 public class exo2 {
+    public static void main(String[] args) {
+        System.out.print("Veuillez entrer deux entiers naturels: ");
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        int N=sc.nextInt();
+        int p=1;
+        for (int i =1;i<=N;i++) {
+            p*=X;
+        }
+        System.out.print("La valeur de X^N est: "+p);
+    }
 }
