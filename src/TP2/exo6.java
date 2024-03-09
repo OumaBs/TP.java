8d323114acc1a787cfd7756625d88ed099f501ff
Ouma <oumayma.boumous@gmail.com>
2024-03-09 01:29:56 +0100

    TP

--- src/TP2/exo6.java
+++ src/TP2/exo6.java
@@ -2,41 +2,43 @@ package TP2;
 import java.util.Scanner;
 public class exo6 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         System.out.print("Entrez un entier positif N: ");
-        int N = sc.nextInt();
+        int N=sc.nextInt();
         sc.close();
-        boolean a = true;
-        if (N <= 1) {
-            a = false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(N); i++) {
-                if (N % i == 0) {
-                    a = false;
+        /*System.out.println("Ce programme teste si un nombre est premier ou pas");
+        boolean p=true;
+        if (N<=1){
+            p=false;
+        }
+        else {
+            for (int i=2;i<= Math.sqrt(N);i++) {
+                if (N%i==0){
+                    p=false;
                     break;
                 }
             }
         }
-        if (a) {
-            System.out.println(N + " est un nombre premier.");
-        } else
-            System.out.println(N + " n'est pas un nombre premier.");
-       /* System.out.println("Liste des nombres premiers entre 1 et " + N + " :");
+        if (p)
+            System.out.println("Le nombre "+N+" est un nombre premier");
+        else
+            System.out.println("Le nombre "+N+" n'est pas un nombre premier");*/
+       System.out.println("Ce programme teste tous les nombres entre 1 et N");
+       System.out.println("La liste des nombres premiers entre 1 et "+N+": ");
         for (int i=2;i<=N;i++) {
-            boolean a=true;
-            if(i<=1){
-                a = false;
-            }
+            boolean p=true;
+            if(i<=1)
+                p=false;
             else {
-                for (int j = 2; j <= Math.sqrt(i); j++) {
-                    if (i % j == 0) {
-                        a = false;
+                for (int j=2;j<=Math.sqrt(i);j++) {
+                    if (i%j==0){
+                        p=false;
                         break;
                     }
                 }
             }
-            if (a)
-                System.out.print(i + " ");
-        }*/
+            if (p)
+                System.out.print(i+" ");
+        }
     }
 }
